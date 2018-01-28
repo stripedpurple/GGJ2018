142cf39085039691ce036e4589556943ae1569dd
DyDyPolarBear <dyannahausenockers@gmail.com>
2018-01-28 12:53:49 -0500

    coins work

--- GGJ2018/Assets/Scripts/Collectible.cs
+++ GGJ2018/Assets/Scripts/Collectible.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collectible : MonoBehaviour {
+public class Collectible : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
     void OnTriggerEnter2D(Collider2D target)
     {
         if (target.gameObject.tag == "Player")
