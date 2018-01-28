e8038d92ebfb7320cc37fba4957230286a3c41d2
DyDyPolarBear <dyannahausenockers@gmail.com>
2018-01-27 23:07:23 -0500

    New Animations &camerafollow

--- GGJ2018/Assets/Scripts/Player.cs
+++ GGJ2018/Assets/Scripts/Player.cs
@@ -57,11 +57,13 @@ public class Player : MonoBehaviour {
 
                 renderer2D.flipX = forceX < 0; //flip when changing direction line 1
             }
-            animator.SetBool("PlayerMove", true);
+            //animator.SetBool("PlayerMove", true);
+            animator.SetInteger("Animator", 1);
         }
         else
         {
-            animator.SetBool("PlayerMove", false);
+            //animator.SetBool("PlayerMove", false);
+            animator.SetInteger("Animator", 3);
         }
 
         if (controller.moving.y > 0)
@@ -69,13 +71,14 @@ public class Player : MonoBehaviour {
             if (absVelY < maxVelocity.y)
             {
                 forceY = jumpSpeed * controller.moving.y;
+                animator.SetInteger("Animator", 2);
             }
 
-            animator.SetBool("PlayerMove", true);
+            //animator.SetBool("PlayerMove", true);
         }
         else if (absVelY > 0 && !standing)
         { //animates falling after a jump
-            animator.SetBool("PlayerMove", false);
+            //animator.SetBool("PlayerMove", false);
         }
 
         body2D.AddForce(new Vector2(forceX, forceY));
