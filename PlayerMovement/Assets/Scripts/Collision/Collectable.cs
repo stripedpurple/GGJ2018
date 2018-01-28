01f56e5630360a3b2784856c02e08143a2709ab9
Austin Barrett <abarrett@biohitech.com>
2018-01-28 11:43:40 -0500

    probably fucked this up part 2

---
+++ PlayerMovement/Assets/Scripts/Collision/Collectable.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collectable : MonoBehaviour {
+
+	public string targetTag = "Player";
+
+	void OnTriggerEnter2D(Collider2D target){
+		if (target.gameObject.tag == targetTag) {
+			OnCollect(target.gameObject);
+			OnDestroy();
+		}
+	}
+
+	protected virtual void OnCollect(GameObject target){
+
+	}
+
+	protected virtual void OnDestroy(){
+		Destroy (gameObject);
+	}
+}
